8f2bc07d25782b52aa86f66cd07a92ac03b85662
Richard Guan <rguan72@gmail.com>
2021-01-01 20:26:44 -0500

    Convert to normal authorization instead of pkce

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const admin = require('firebase-admin')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const axios = require('axios')
+const qs = require('querystring')
+require('dotenv').config()
 const serviceAccount = require("./spotify-mixer-7dff8-firebase-adminsdk-1nxb7-33f2871a10.json")
 const app = express()
 const port = 3000
@@ -29,6 +32,32 @@ app.post('/token', (req, res) => {
         .catch((error) => {
             console.log('Error creating token: ', error)
         })
+})
+
+app.post('/spotify/token', async (req, res) => {
+    const code = req.body.code
+    const requestBody = {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,   
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: process.env.REDIRECT_URI,
+    }
+    console.log(requestBody)
+    const config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }
+    const url = "https://accounts.spotify.com/api/token"
+    axios.post(url, qs.stringify(requestBody), config).then(spotifyRes => {
+        res.send(spotifyRes.data)
+    }).catch((err) => {
+        if (err.status_code)
+            res.status(err.status_code).send(err)
+        else 
+            res.status(500).send(err)
+    })
 })
 
 app.listen(port, () => {
